a0d1ad56cc3168b5e4c606a2d1ae074eaa81785b
simituli <anupama.tuli@gmail.com>
2024-10-19 14:17:48 -0700

    Update Sample.java
    Add solution for: https://leetcode.com/problems/decode-string/

--- Sample.java
+++ Sample.java
@@ -1,7 +1,42 @@
-// Time Complexity :
-// Space Complexity :
+// Time Complexity : O(Length * product of all integers: K)
+// Space Complexity :O(Length * product of all integers: K)
 // Did this code successfully run on Leetcode :
 // Any problem you faced while coding this :
 
 
 // Your code here along with comments explaining your approach
+//DFS
+class Solution {
+    int i =0;
+    public String decodeString(String s) {
+        int currNum = 0;
+        StringBuilder currStr = new StringBuilder();
+        while(i < s.length())
+        {
+            char c = s.charAt(i);
+            i++;
+            if(Character.isDigit(c))
+            {
+                currNum = currNum*10+c-'0';
+            }
+            else if(c=='[')
+            {
+                String baby = decodeString(s);
+                for(int k = 0;k<currNum;k++)
+                {
+                    currStr.append(baby);
+                }
+                currNum=0;
+            }
+            else if(c==']')
+            {
+                return currStr.toString();
+            }
+            else
+            {
+                currStr.append(c);
+            }
+        }
+        return currStr.toString();
+    }
+}
