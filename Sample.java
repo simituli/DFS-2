e9d558b0f566c59866c12e89df0b020a3c07ac84
simituli <anupama.tuli@gmail.com>
2024-10-19 15:46:34 -0700

    Update Sample.java
    Update the DFS version of https://leetcode.com/problems/number-of-islands/

--- Sample.java
+++ Sample.java
@@ -40,3 +40,43 @@ class Solution {
         return currStr.toString();
     }
 }
+
+// Time Complexity : O(m*n)
+// Space Complexity :O(m*n) for DFS (it was min(m,n) in case BFS
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : yes. syntax error plus logical error
+//DFS
+class Solution {
+    int m;
+    int n;
+    int [][] dirs;
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        m = grid.length;
+        n = grid[0].length;
+        dirs = new int[][]{{-1,0},{0,1},{1,0},{0,-1}};
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j]=='1')
+                {
+                    count++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+    private void dfs(char[][] grid, int i,int j)
+    {
+        if(i<0||j<0||i>=m||j>=n||grid[i][j]!='1') return;
+        grid[i][j] = '0';
+        for(int[] dir:dirs)
+        {
+            int r = dir[0]+i;
+            int c= dir[1]+j;
+            dfs(grid, r, c);
+        }
+    }
+}
